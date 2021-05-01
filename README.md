492e62a26508567114d4e1b50247f35543743cf3
Umesh <umeshsarkar19@outlook.com>
2021-05-01 21:41:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# pokedex
+
+A new Flutter project.
+
+## Getting Started
+
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokeddex',
+      theme: ThemeData(
+      
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
