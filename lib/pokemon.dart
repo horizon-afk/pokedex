23f05739b421afbad5bfa22cd5a030ca80836742
Umesh <umeshsarkar19@outlook.com>
2021-05-01 23:31:51 +0530

    Created a pokemon class

---
+++ lib/pokemon.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Pokemon {
+  String name;
+  String type;
+  Image image;
+  int height;
+  int weight;
+
+  Pokemon({
+    this.name,
+    this.type,
+    this.image,
+    this.height,
+    this.weight,
+  });
+}
