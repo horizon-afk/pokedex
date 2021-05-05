253da7e5fbd0037eb3ee4930de7a003d6528b270
Umesh <umeshsarkar19@outlook.com>
2021-05-05 23:40:30 +0530

    Added lists to show in main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -12,8 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Pokeddex',
       theme: ThemeData(
-          primarySwatch: Colors.red, 
-          scaffoldBackgroundColor: Colors.red),
+          primarySwatch: Colors.red, scaffoldBackgroundColor: Colors.red),
       home: MyHomePage(),
     );
   }
@@ -22,6 +22,48 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Pokedex")));
+    return Scaffold(
+      appBar: AppBar(title: Text("Pokedex")),
+      body: PokeList(),
+    );
+  }
+}
+
+class PokeList extends StatefulWidget {
+  @override
+  _PokeListState createState() => _PokeListState();
+}
+
+class _PokeListState extends State<PokeList> {
+  List<String> pokemonList = [];
+
+  void getPokemon() async {
+    final url = Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=898');
+    http.Response request = await http.get(url);
+    dynamic response = jsonDecode(request.body);
+
+    const pokemons = 898;
+
+    for (int i = 0; i < pokemons; i++) {
+      String pokemon = response['results'][i]['name'];
+      pokemonList.add(pokemon);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getPokemon();
+    return Container(
+        child: ListView.builder(
+      itemCount: 898,
+      itemBuilder: (context, index) {
+        return ListTile(
+            title: Text(
+          '#${index + 1}  ${pokemonList[index]}',
+          textScaleFactor: 1.5,
+          style: TextStyle(color: Colors.white),
+        ));
+      },
+    ));
   }
 }
