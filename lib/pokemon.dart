7e75a204e3f00ed4291ba49ee3f9359e96e3d945
Umesh <umeshsarkar19@outlook.com>
2021-05-24 22:08:13 +0530

    updated the info of typical pokemon

--- lib/pokemon.dart
+++ lib/pokemon.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Pokemon {
+  int index;
   String name;
   String type;
+  String description;
   Image image;
   int height;
   int weight;
 
   Pokemon({
+    this.index,
     this.name,
     this.type,
+    this.description,
     this.image,
     this.height,
     this.weight,
