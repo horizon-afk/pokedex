f5a7bfe1bed6897879ae6da42e6621656bd90244
Umesh <umeshsarkar19@outlook.com>
2021-08-25 21:52:39 +0530

    Added search feature to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -21,23 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Pokeddex',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // backgroundColor: Colors.blue[700],
-      appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Text(
-            "Pokedex",
-            style: TextStyle(color: Colors.black),
-          )),
-      body: PokeList(),
+      home: PokeList(),
     );
   }
 }
@@ -77,41 +62,102 @@ class _PokeListState extends State<PokeList> {
     return pokemonList;
   }
 
+  Widget appBarTitle = Text("Pokedex", style: TextStyle(color: Colors.black));
+  Icon actionIcon = Icon(
+    Icons.search,
+    color: Colors.black,
+  );
+
+  Widget buildBar(BuildContext context, List<String> pokelist) {
+    return new AppBar(backgroundColor: Colors.white, title: appBarTitle, actions: [
+      IconButton(
+          icon: this.actionIcon,
+          onPressed: () {
+            setState(() {
+              if (this.actionIcon.icon == Icons.search) {
+                this.actionIcon = Icon(Icons.close, color: Colors.black);
+
+                this.appBarTitle = TextField(
+                  style: TextStyle(color: Colors.black),
+                  onChanged: (text) {
+                    text = text.toLowerCase();
+                    setState(() {
+                      filteredList.clear();
+                      searching = true;
+                      searchFilter(text, pokelist);
+                      if (text == "") {
+                        searching = false;
+                      }
+                    });
+                  },
+                );
+              } else {
+                this.actionIcon = Icon(
+                  Icons.search,
+                  color: Colors.black,
+                );
+                searching = false;
+                this.appBarTitle = Text("Pokedex", style: TextStyle(color: Colors.black));
+              }
+            });
+          })
+    ]);
+  }
+
+  bool searching = false;
+  List<String> filteredList = [];
+
+  void searchFilter(String query, List<String> pokemon) {
+    for (int i = 0; i < 898; i++) {
+      if (pokemon[i].startsWith(query)) {
+        filteredList.add(pokemon[i]);
+      }
+    }
+
+    for (int i = 0; i < 898; i++) {
+      if (pokemon[i].contains(query) && !filteredList.contains(pokemon[i])) {
+        filteredList.add(pokemon[i]);
+      }
+    }
+  }
+
+  Widget mainList(int length, List maindata, List data) {
+    return ListView.builder(
+        itemCount: length,
+        itemBuilder: (context, index) {
+          return Card(
+              elevation: 2,
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.black12, width: 1), borderRadius: BorderRadius.circular(15.0)),
+              child: Container(
+                  child: InkWell(
+                      splashColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => PokeInfo(data[index])));
+                      },
+                      child: ListTile(
+                        title: Text(
+                          '#${maindata.indexOf(data[index]) + 1} ${data[index]}',
+                          textScaleFactor: 1.5,
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ))));
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
         child: FutureBuilder(
-      future: getPokemon(),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.hasData) {
-          return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (context, index) {
-                return Card(
-                    elevation: 2,
-                    shape: RoundedRectangleBorder(
-                        side: BorderSide(color: Colors.black12, width: 1), borderRadius: BorderRadius.circular(15.0)),
-                    child: Container(
-                        child: InkWell(
-                            splashColor: Colors.transparent,
-                            highlightColor: Colors.transparent,
-                            onTap: () {
-                              Navigator.push(
-                                  context, MaterialPageRoute(builder: (context) => PokeInfo(snapshot.data[index])));
-                            },
-                            child: ListTile(
-                              title: Text(
-                                '#${index + 1} ${snapshot.data[index]}',
-                                textScaleFactor: 1.5,
-                                style: TextStyle(color: Colors.black),
-                              ),
-                              onTap: null,
-                            ))));
-              });
-        } else {
-          return Center(child: CircularProgressIndicator());
-        }
-      },
-    ));
+            future: getPokemon(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              return Scaffold(
+                  appBar: buildBar(context, snapshot.data),
+                  body: snapshot.hasData
+                      ? mainList(!searching ? snapshot.data.length : filteredList.length, snapshot.data,
+                          !searching ? snapshot.data : filteredList)
+                      : Center(child: CircularProgressIndicator()));
+            }));
   }
 }
