35614907dd095e19da20fa01e5b6477e0b31f17c
Umesh <umeshsarkar19@outlook.com>
2021-09-08 21:55:34 +0530

    Made changes to pokemon.dart

--- lib/pokemon.dart
+++ lib/pokemon.dart
@@ -2,18 +2,11 @@ class Pokemon {
   final int index;
   final String name;
   final String type;
-  final String description;
+  String description;
   final String image;
   final int height;
   final int weight;
+  final List evolution;
 
-  Pokemon({
-    this.index,
-    this.name,
-    this.type,
-    this.description,
-    this.image,
-    this.height,
-    this.weight,
-  });
+  Pokemon({this.index, this.name, this.type, this.description, this.image, this.height, this.weight, this.evolution});
 }
